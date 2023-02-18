92841365df245b0247d4dbb65ee447aa464fc72d
mvtran562 <mvtran@scu.edu>
2023-02-17 22:44:42 -0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:DateDash/page/home_page.dart';
+import 'package:DateDash/page/login_page.dart';
+import 'package:DateDash/provider/feedback_position_provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (context) => FeedbackPositionProvider(),
+        child: MaterialApp(
+          title: 'Tinder Swiping',
+          theme: ThemeData(
+            primarySwatch: Colors.deepOrange,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          home: LoginPage(),
+        ),
+      );
+}
